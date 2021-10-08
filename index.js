5ba2f23b6eaa9fa107194fa82c9db550b249c60c
Roger Valencia <roger.valencia.garcia@hotmail.com>
2021-10-08 15:36:12 -0500

    Configuramos el string de conexion y inializamos el servidor para su escucha

---
+++ index.js
@@ -0,0 +1,21 @@
+import Express from 'express';
+import Cors from 'cors';
+import dotenv from 'dotenv';
+import { connectDatabase } from './model/database.js';
+
+//Nos permite usar los archivos .env para tener las rutas en otro lado no visibles en el repo
+dotenv.config({ path: './.env' });
+
+const app = Express();
+
+app.use(Express.json());
+//Nos permite decirle al servidor que puede recibir peticiones desde cualqueir localizacion, movil, web, etc.
+app.use(Cors());
+
+const principal = () => {
+	return app.listen(process.env.PORT, () => {
+		console.log(`Trabajando en el puerto ${process.env.PORT}`);
+	});
+};
+
+connectDatabase(principal);
