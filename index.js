7a347ddfa4475dc8e9fd30c8db6257718a6ab92e
Roger Valencia <roger.valencia.garcia@hotmail.com>
2021-10-20 11:39:21 -0500

    Implementando middleware de Auth0

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import { connectDatabase } from './src/model/database.js';
 import rutasProducto from './src/router/productos/rutas.productos.js';
 import rutasVentas from './src/router/ventas/rutas.ventas.js';
 import rutasUsuario from './src/router/usuarios/rutas.usuarios.js';
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
 
 //Nos permite usar los archivos .env para tener las rutas en otro lado no visibles en el repo
 dotenv.config({ path: './.env' });
@@ -15,6 +17,20 @@ app.use(Express.json());
 
 //Nos permite decirle al servidor que puede recibir peticiones desde cualquier localización, movil, web, etc.
 app.use(Cors());
+
+var jwtCheck = jwt({
+	secret: jwks.expressJwtSecret({
+		cache: true,
+		rateLimit: true,
+		jwksRequestsPerMinute: 5,
+		jwksUri: 'https://ingeneomisionticciclo3ecommerceproject.us.auth0.com/.well-known/jwks.json'
+	}),
+	audience: 'API-Ecommerce-Project',
+	issuer: 'https://ingeneomisionticciclo3ecommerceproject.us.auth0.com/',
+	algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
 
 //Los middleware de las diferentes Endpoints del aplicativo
 app.use(rutasVentas);
